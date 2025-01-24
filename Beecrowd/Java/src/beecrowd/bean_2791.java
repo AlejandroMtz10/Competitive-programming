7b447b597fb9b43f11da9dabf6aa2f4dfaab8e56
AlejandroMtz10 <alex10mtz_rivera@outlook.com>
2025-01-23 21:09:53 -0600

    I add the solutions 2791 and 1340 of beecrowd with Java
    I add the solutions 2791 and 1340 of beecrowd with Java

---
+++ Beecrowd/Java/src/beecrowd/bean_2791.java
@@ -0,0 +1,36 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class bean_2791 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int c1, c2, c3, c4;
+		
+		c1 = sc.nextInt();
+		c2 = sc.nextInt();
+		c3 = sc.nextInt();
+		c4 = sc.nextInt();
+		
+		if(c1 == 1) {
+			System.out.println(1);
+		}
+		
+		if(c2 == 1) {
+			System.out.println(2);
+		}
+		
+		if(c3 == 1) {
+			System.out.println(3);
+		}
+		
+		if(c4 == 1) {
+			System.out.println(4);
+		}
+		
+	}
+
+}
