14347007cab00b4f300132c01222e8691c595002
AlejandroMtz10 <alex10mtz_rivera@outlook.com>
2025-01-26 12:13:00 -0600

    Create domino_3046.java solution of beecrowd

---
+++ Beecrowd/Java/src/beecrowd/domino_3046.java
@@ -0,0 +1,16 @@
+package beecrowd;
+
+import java.util.*;
+
+public class domino_3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		n = ((n+1) * (n+2)) / 2;
+		
+		System.out.println(n);
+	}
+	
+}
