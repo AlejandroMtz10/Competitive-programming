1792ba744e405f86d5bac579e4999a04ba4a5aa9
AlejandroMtz10 <alex10mtz_rivera@outlook.com>
2025-02-27 18:17:50 -0600

    I add a new solution of beecrowd 1933
    I add a new solution of beecrowd 1933, I solved this problem with java

---
+++ Beecrowd/Java/src/beecrowd/tri_du_1933.java
@@ -0,0 +1,24 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class tri_du_1933 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = 0, b = 0;
+		
+		a = sc.nextInt();
+		
+		b = sc.nextInt();
+		
+		if(a >= b) {
+			System.out.println(a);
+		}else {
+			System.out.println(b);
+		}
+	}
+
+}
