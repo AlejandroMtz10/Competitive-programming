74ff5ed6dfb4b36ac08a91e9a3c18ea46821657f
AlejandroMtz10 <alex10mtz_rivera@outlook.com>
2025-04-06 16:45:27 -0600

    I add new solutions in beecrowd with java, c++ and sql
    I add new solutions in beecrowd with java, c++ and sql, there are 3 new solutions

---
+++ Beecrowd/Java/src/beecrowd/output_6_2752.java
@@ -0,0 +1,19 @@
+package beecrowd;
+
+public class output_6_2752 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String phrase = "AMO FAZER EXERCICIO NO URI";
+		
+        System.out.println("<" + phrase + ">");
+        System.out.println("<" + String.format("%30s", phrase) + ">");
+        System.out.println("<" + String.format("%.20s", phrase) + ">");
+        System.out.println("<" + String.format("%-20s", phrase) + ">");
+        System.out.println("<" + String.format("%-30s", phrase) + ">");
+        System.out.println("<" + String.format("%.30s", phrase) + ">");
+        System.out.println("<" + String.format("%30.20s", phrase) + ">");
+        System.out.println("<" + String.format("%-30.20s", phrase) + ">");
+	}
+
+}
