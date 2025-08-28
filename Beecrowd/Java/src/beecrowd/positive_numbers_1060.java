73d177b71334cd7779065505e084904fc5109c12
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2025-08-27 20:45:47 -0500

    Add soluton positive numbers 1060 with java in beecrowd

---
+++ Beecrowd/Java/src/beecrowd/positive_numbers_1060.java
@@ -0,0 +1,27 @@
+package beecrowd;
+
+import java.util.*;
+
+public class positive_numbers_1060 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner sc = new Scanner(System.in);
+
+        double numbers[] = new double[6];
+        int count = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextDouble();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count + " valores positivos");
+    }
+}
