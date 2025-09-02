7325b4000e0ac284229d51965073dbfacd93adb0
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2025-09-02 16:18:51 -0500

    (feat) Add the solution forgotten note 3055 of beecrowd with java

---
+++ Beecrowd/Java/src/beecrowd/forgotten_note_3055.java
@@ -0,0 +1,21 @@
+package beecrowd;
+import java.util.Scanner;
+public class forgotten_note_3055 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = 0, b = 0, m = 0;
+
+        a = sc.nextInt();
+
+        m = sc.nextInt();
+
+        b = (m * 2) - a;
+
+        System.out.println(b);
+
+    }
+
+}
