d43f338c54120806b289725e7748e8acf0cf02fd
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2025-12-05 16:17:55 -0600

    Feat: Add the solution 1095 of beecrowd
    Feat: Add the solution secuence IJ 1 problem 1095 of beecrowd with java

---
+++ Beecrowd/Java/src/beecrowd/secuence_IJ_1095.java
@@ -0,0 +1,11 @@
+package beecrowd;
+
+public class secuence_IJ_1095 {
+    public static void main (String[] args){
+        int i = 1;
+        for (int j = 60; j >= 0; j -= 5){
+            System.out.println("I="+ i +" J="+ j);
+            i += 3;
+        }
+    }
+}
