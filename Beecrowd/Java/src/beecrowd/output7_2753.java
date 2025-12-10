df9ded38a6fe0e83ab2558304628054003b1eee1
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2025-12-09 20:56:04 -0600

    Feat: Add solution of the problem output 7 of  beecrowd
    Feat: Add solution of the problem output 7 number 2753 of  beecrowd, this solution was written in java

---
+++ Beecrowd/Java/src/beecrowd/output7_2753.java
@@ -0,0 +1,12 @@
+package beecrowd;
+
+public class output7_2753 {
+    public static void main(String[] args) {
+        int count = 97;
+        // Bucle to iterate over the array
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            System.out.println(count + " e " + letter); // Print number and letters
+            count++;
+        }
+    }
+}
