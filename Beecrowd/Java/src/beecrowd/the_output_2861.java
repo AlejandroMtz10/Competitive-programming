24a1c27feab5b8a6e98d20eddefe3a31799df3df
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2026-01-15 21:07:28 -0600

    Feat: Add solution the output 2861 in Java
    Feat: Add solution the output 2861 of beecrowd, it was solved in Java

---
+++ Beecrowd/Java/src/beecrowd/the_output_2861.java
@@ -0,0 +1,20 @@
+package beecrowd;
+import java.util.Scanner;
+public class the_output_2861 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int nCases = 0;
+        String question = "";
+
+        nCases = sc.nextInt();
+
+        for(int i =0; i < nCases; i++){
+            question = sc.next();
+        }
+
+        for(int i =0; i < nCases; i++){
+            System.out.println("gzuz");
+        }
+    }
+}
