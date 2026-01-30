bd88d28af0108db30f3369f19e636a558c368f00
Alejandro Martínez Rivera <alex10mtz_rivera@outlook.com>
2026-01-29 20:05:41 -0600

    Feat: New project introduction java of hackerrank
    Feat: New project introduction java of hackerrank, add three solutions in java: welcome to java, java Stdin & Stdout I and  Java if-else

---
+++ Hackerrank/hackerrack_java/introduction_java/src/stdin_and_stdout_I.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class stdin_and_stdout_I {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = 0, n2 = 0, n3 = 0;
+
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        n3 = sc.nextInt();
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+    }
+}
